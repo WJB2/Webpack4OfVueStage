bb1b41e221267967d29f014b813f042180ae0fe5
WJB6666 <wujiabao3675@dingtalk.com>
2018-12-28 16:46:59 +0800

    更新babel版本

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';//在import Vue的过程中，Vue主要是在原型上完�
 import App from './app.vue';//引入主VUE页面
 import "./utils/Rem";//引入将根页面的fontsize修改后 直接就可以在项目中使用rem实现自适应
 
+import './styles/reset.css' /**引入样式重置 */
+
+Vue.config.productionTip = false;
+
 new Vue({
     el:"#root",//将渲染结果挂在这id为root的html上
     render:h=>h(App),//render函数是渲染一个视图，然后提供给el挂载，如果没有render那页面什么都不会出来
